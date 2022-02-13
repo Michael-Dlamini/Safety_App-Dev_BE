48d77fd83413b23f24b55aadc6c75e4f537c7c20
ASZ000717 <andilesiyanda2000@gmail.com>
2022-02-13 16:44:40 +0200

    ,,

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const router = require('./routes/reportapi');
+const router = require('./routes/vecapi');
 const app = express();
 const port = process.env.PORT || 5001
 
